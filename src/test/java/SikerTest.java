de64a9260abb1cdca1d0a64078307374e7231156
JonaszJestem <jonaszwiacek@gmail.com>
2018-04-21 21:12:09 +0200

    Added test for multiple searchers

--- src/test/java/SikerTest.java
+++ src/test/java/SikerTest.java
@@ -1,23 +1,31 @@
 import Siker.Searchers.Item;
 import Siker.Searchers.OlxSearcher;
+import Siker.Searchers.SprzedajemySearcher;
+import Siker.Siker;
+import org.junit.Ignore;
 import org.junit.Test;
-import Siker.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
-public class OlxSearcherTest {
+public class SikerTest {
     @Test
     public void shouldReturnResultSetFromGivenSearch() {
+        ArrayList<String> searchPhrases = new ArrayList<>(Arrays.asList("komputer","czesci", "telefon", "samochod", "telewizor", "ksiazka"));
         Siker siker = new Siker();
         siker.addSearcher(new OlxSearcher());
-
-        ArrayList<String> searchPhrases = new ArrayList<>(Arrays.asList("komputer","czesci", "telefon", "samochod", "telewizor", "ksiazka"));
+        siker.addSearcher(new SprzedajemySearcher());
 
         for(int i = 0; i < 1; i++) {
             ArrayList<Item> items = new ArrayList<>(siker.search(searchPhrases.get(i%searchPhrases.size())));
+
+            for (Item item: items
+                    ) {
+                System.out.println(item.getImageSource());
+            }
+
             assertFalse(items.isEmpty());
         }
     }
